6c94f9255dfe4bd2ae41f09f44405a2c6dea685e
Vincent Toon <vincenttoon22@gmail.com>
2023-10-30 14:13:48 -0500

    skeleton added

--- src/Components/Modal.js
+++ src/Components/Modal.js
@@ -2,7 +2,16 @@ import React from 'react'
 
 function Modal() {
   return (
-    <div>Modal</div>
+    <div className='modalBackground'>
+        <div className='modalContainer'>
+            <button> X </button>
+            <div className='title'>
+                <h1>Are you sure you want to continue</h1>
+            </div>
+            <div className='body'></div>
+            <div className='footer'></div>
+        </div>
+    </div>
   )
 }
 
